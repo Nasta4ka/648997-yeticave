a685f1550053fcd70d71ff5b76e0dd52c838228f
Nasta4ka <firago.a.i@mail.ru>
2019-03-05 13:27:24 +0300

    задание 14

--- templates/login.php
+++ templates/login.php
@@ -22,14 +22,21 @@
             </form>
             <a class="main-header__add-lot button" href="add.php">Добавить лот</a>
             <nav class="user-menu">
-                <ul class="user-menu__list">
-                    <li class="user-menu__item">
-                        <a href="sign_up.php">Регистрация</a>
-                    </li>
-                    <li class="user-menu__item">
-                        <a href="login.php">Вход</a>
-                    </li>
-                </ul>
+                <?php if($is_auth === 1): ?>
+                    <div class="user-menu__logged">
+                        <p><?= $user_name; ?></p>
+                    </div>
+                <?php else: ?>
+                    <ul class="user-menu__list">
+                        <li class="user-menu__item">
+                            <a href="sign_up.php">Регистрация</a>
+                        </li>
+                        <li class="user-menu__item">
+                            <a href="login.php">Вход</a>
+                        </li>
+                    </ul>
+                <?php endif; ?>
+
             </nav>
         </div>
     </header>
